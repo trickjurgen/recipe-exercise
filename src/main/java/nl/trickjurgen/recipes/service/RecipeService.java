9a0de24dd7ca053eeafd0c0346efc35260014c3b
Jurgen Rijsdijk <jurgen.rijsdijk@teamrockstars.nl>
2024-06-27 10:58:22 +0200

    docker db and svc func

--- src/main/java/nl/trickjurgen/recipes/service/RecipeService.java
+++ src/main/java/nl/trickjurgen/recipes/service/RecipeService.java
@@ -18,11 +18,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -59,15 +60,14 @@ public class RecipeService {
         if (recipeDto.getId() != null) {
             throw new RecipeParameterException("new recipe should not have an ID");
         }
-        Recipe newRecipe = RecepAndIngrMapper.dtoToRecipeNoIngr(recipeDto);
+        Recipe newRecipe = RecepAndIngrMapper.dtoToRecipeNoIngr(recipeDto); // this includes title-case fix on name
         if (recipeRepo.findByName(newRecipe.getName()).isPresent()) {
             logger.warn("already exists! {}", newRecipe.getName());
-            throw new DuplicateRecipeException("already exist and not update call");
+            throw new DuplicateRecipeException("already exists and this is not an update call");
         }
-        Set<IngredientDto> ingredients = recipeDto.getIngredients();
-        for (IngredientDto ingredientDto : ingredients) {
-            newRecipe.getIngredients().add(createAndSaveIngredient(ingredientDto));
-        }
+        newRecipe.getIngredients().addAll(
+                recipeDto.getIngredients().stream().map(this::createAndSaveIngredient).toList()
+        );
         Recipe saved = recipeRepo.save(newRecipe);
         return RecepAndIngrMapper.recipeToDto(saved);
     }
@@ -98,42 +98,46 @@ public class RecipeService {
     }
 
     private Set<Ingredient> mergeIngredients(Set<Ingredient> savedIngredients, Set<IngredientDto> newIngredients) {
-        HashSet<Ingredient> target = new HashSet<>();
-        List<String> newIngNames = newIngredients.stream().map(IngredientDto::getName).map(NameStringHelper::toTitleCase).toList();
+        final HashSet<Ingredient> mergedIngredients = new HashSet<>();
+        final List<String> newIngrNames = newIngredients.stream().map(IngredientDto::getName).map(NameStringHelper::toTitleCase).toList();
         // update or remove existing
-        for (Ingredient ing: savedIngredients) {
-            if (newIngNames.contains(ing.getIngredientType().getName())) {
-                try {
-                    Ingredient changed = updateIngredient(ing, newIngredients);
-                    ingredientRepo.save(changed);
-                    target.add(changed);
-                } catch (NoSuchElementException e) {
-                    logger.error("failed to update ingredient {}", ing.getIngredientType().getName());
-                    throw new RecipeParameterException("internal update issue");
-                }
+        for (Ingredient ing : savedIngredients) {
+            if (newIngrNames.contains(ing.getIngredientType().getName())) {
+                Ingredient changed = updateIngredientWithDtoFields(ing, newIngredients);
+                ingredientRepo.save(changed);
+                mergedIngredients.add(changed);
             } else {
                 ingredientRepo.delete(ing);
             }
         }
         // add all the new ones
-        List<String> curIngrList = target.stream().map(i -> i.getIngredientType().getName()).toList();
-        newIngredients.stream()
-                .filter(dto -> !curIngrList.contains(NameStringHelper.toTitleCase(dto.getName())))
-                .forEach(dto -> {
-                    Ingredient andSaveIngredient = createAndSaveIngredient(dto);
-                    target.add(andSaveIngredient);
-                });
-        return target;
+        final List<String> curIngrList = mergedIngredients.stream().map(i -> i.getIngredientType().getName()).toList();
+        final Predicate<IngredientDto> nameIsNotInCurrentIngredientList =
+                dto -> !curIngrList.contains(NameStringHelper.toTitleCase(dto.getName()));
+        mergedIngredients.addAll(newIngredients.stream()
+                .filter(nameIsNotInCurrentIngredientList)
+                .map(this::createAndSaveIngredient)
+                .toList());
+        return mergedIngredients;
     }
 
-    private Ingredient updateIngredient(Ingredient ingredient, Set<IngredientDto> newIngredients) throws NoSuchElementException {
-        final String findName = ingredient.getIngredientType().getName();
-        IngredientDto updatedIngredientDto = newIngredients.stream()
-                .filter(i -> i.getName().equalsIgnoreCase(findName))
-                .findFirst().orElseThrow();
+    private Ingredient updateIngredientWithDtoFields(Ingredient ingredient, Set<IngredientDto> newIngredients) {
+        final IngredientDto updatedIngredientDto = findMatchingDtoByName(newIngredients, ingredient.getIngredientType().getName());
         ingredient.setVolume(updatedIngredientDto.getVolume());
         ingredient.setRemark(updatedIngredientDto.getRemark());
         return ingredient;
+    }
+
+    private IngredientDto findMatchingDtoByName(Set<IngredientDto> newIngredients, String nameToFind) {
+        final Predicate<IngredientDto> matchByName = ingredientDto -> ingredientDto.getName().equalsIgnoreCase(nameToFind);
+        return newIngredients.stream()
+                .filter(matchByName)
+                .findFirst()
+                .orElseThrow(() -> {
+                    // this is very unlikely to happen as we did a range check in 'newIngredients' before starting the update
+                    logger.error("failed to update ingredient {}", nameToFind);
+                    return new RecipeParameterException("internal update issue");
+                });
     }
 
     public boolean deleteRecipe(final Long recipeId) {
@@ -143,7 +147,7 @@ public class RecipeService {
         try {
             Recipe storedRecipe = recipeRepo.getReferenceById(recipeId);
             storedRecipe.getIngredients().forEach(ingredientRepo::delete);
-            // TODO this could leave ingredientTypes unused, maybe cleanup of those here?
+            // FIXME this could leave ingredientTypes unused, maybe cleanup of those here?
             recipeRepo.delete(storedRecipe);
         } catch (IllegalArgumentException e) {
             logger.error("failed to delete (part of) recipe with ID {}", recipeId);
@@ -152,8 +156,40 @@ public class RecipeService {
         return true;
     }
 
-    public List<RecipeDto> findRecipesWithSpecificIngredients(final List<String> includes, final List<String> excludes) {
-        // TODO this
+    public List<RecipeDto> findRecipesWithSpecificDetails(final Boolean isVeggie, final Integer minServing, final Integer maxServing,
+                                                          final List<String> includes, final List<String> excludes) {
+        List<Recipe> allRecipes = recipeRepo.findAll();
+
+        List<Predicate<Recipe>> filters = new ArrayList<>();
+        Optional.ofNullable(isVeggie).ifPresent(bool -> filters.add(recipe -> recipe.isVegetarian() == bool));
+        Optional.ofNullable(minServing).ifPresent(minVal -> filters.add(recipe -> recipe.getServings() >= minVal));
+        Optional.ofNullable(maxServing).ifPresent(maxVal -> filters.add(recipe -> recipe.getServings() <= maxVal));
+
+        if (null != includes) {
+            filters.addAll(
+                includes.stream()
+                            .map(NameStringHelper::toTitleCase)
+                            .map(titleCase -> (Predicate<Recipe>) recipe -> flatListIngredients(recipe).contains(titleCase) )
+                            .toList()
+            );
+        }
+
+        if (null != excludes) {
+        for (String ingredientName : excludes) {
+                final String titleCase = NameStringHelper.toTitleCase(ingredientName);
+                filters.add(recipe -> !flatListIngredients(recipe).contains(titleCase));
+            }
+        }
+
+        Predicate<Recipe> combinedFilter = filters.stream().reduce(Predicate::and).orElse(x -> true);
+        List<Recipe> filteredRecipes = allRecipes.stream().filter(combinedFilter).toList();
+        return filteredRecipes.stream()
+                .map(RecepAndIngrMapper::recipeToDto)
+                .collect(Collectors.toList());
+    }
+
+    List<String> flatListIngredients(final Recipe recipe){
+        return recipe.getIngredients().stream().map(ingredient -> ingredient.getIngredientType().getName()).toList();
     }
 
 }
